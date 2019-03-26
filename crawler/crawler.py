9abc02d983a4e2601132ecff8fd4a8784074f6b7
Jong Hyun Park <jonhpark7966@gmail.com>
2019-03-26 18:46:12 +0900

    Ppomppu cralwer & timer.
    crawling from ppomppu,
    filtered lists by latest list number.
    using it from timer.

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -8,11 +8,15 @@ class Crawler:
         assert(0); # pure virtual!
 
 
+# NOTES: found_list_ consists of "list number (int)", "title", "link"
+# USAGE: just call "find(KEYWORD)"
 class PpCrawler:
     def __init__(self):
         self.url = 'http://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu'
+        self.found_list_ = []
 
     def find(self, keyword):
+        self.found_list_ = []
 
         soup = BeautifulSoup(requests.get(self.url).text, 'html.parser')
         raw_lists = soup.find_all("tr", {"class": "list1"})
@@ -22,11 +26,10 @@ class PpCrawler:
 
         for li in raw_lists:
             title = (li.find("font").text)
-            if keyword in title:
-                #TODO: do something!
-                print(title)
-            registered_time = (li.find("nobr", {"class": "eng list_vspace"}).text)
+            #registered_time = (li.find("nobr", {"class": "eng list_vspace"}).text)
             link = (li.find("td", {"valign":"middle"}).find("a")["href"])
             list_num = li.find("td", {"class", "eng list_vspace"}).text
+            if keyword in title:
+                self.found_list_.append([int(list_num), title, link])
+        return self.found_list_
 
-        return list_num
--- timer.py
+++ timer.py
@@ -3,9 +3,29 @@
 from time import sleep
 from crawler.crawler import PpCrawler
 
+def filterLatest(latest_num, found_list):
+    ret = []
+    tmp_max = latest_num[0]
+    for elm in found_list:
+        if elm[0] > latest_num[0]:
+            ret.append(elm)
+            tmp_max = max([tmp_max,elm[0]])
+
+    latest_num[0] = tmp_max
+    return ret
+
+
 #PPOMPPU!
 pp = PpCrawler()
+pp_latest = [-1]
 
 while True:
-    pp.find("상품권")
-    sleep(5)
+    found_list = pp.find("상품권")
+    filtered_list = filterLatest(pp_latest, found_list)
+
+    #for test!
+    print(pp_latest)
+    for elm in filtered_list:
+        print(elm)
+
+    sleep(20)
